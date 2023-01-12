1e08f15714fc42c9e9e6fd0c189d14078edae270
Oleksandr Romanchuk <novator0511@gmail.com>
2023-01-12 16:00:57 +0200

    Update FeedbackOptions.styled.jsx

--- src/components/FeedbackOptions/FeedbackOptions.styled.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.styled.jsx
@@ -2,16 +2,27 @@ import styled from "styled-components";
 
 export const FeedbackBtnsList = styled.ul`
     display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    gap: ${({ theme }) => theme.space * 3}px;
-
-    margin-top: ${({theme}) => theme.space * 8}px;
+    grid-template-columns: repeat(1, 1fr);
+    gap: 12px;
 `;
 
 export const FeedbackBtn = styled.button`
-    min-width: 120px;
+    min-width: 90px;
 
-    padding: ${({ theme }) => theme.space * 2}px ${({ theme }) => theme.space * 4}px;
+    padding: 8px 12px;
     
-    font-size: ${({theme}) => theme.fontSizes[2]}px;
+    font-size: 14px;
+    
+    background-color: #ffffff;
+    border: none;
+    outline: none;
+    border-radius: 6px;
+
+    transition: background-color 300ms ease, box-shadow 300ms ease;
+
+    :hover, :focus {
+        background-color: #64e3ff;
+
+        box-shadow: 0 0 16px #64e3ff;
+    }
 `;
