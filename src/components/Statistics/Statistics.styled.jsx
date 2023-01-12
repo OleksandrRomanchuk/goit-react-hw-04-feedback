6058c425c1787bc07174125fcc4add9c2dea401c
Oleksandr Romanchuk <novator0511@gmail.com>
2023-01-12 16:01:03 +0200

    Update Statistics.styled.jsx

---
+++ src/components/Statistics/Statistics.styled.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const FeedbackList = styled.ul`
+    display: grid;
+    gap: 8px;
+    
+    font-size: 16px;
+`;
+
+export const FeedbackStatList = styled.ul`
+    display: grid;
+    gap: 8px;
+    
+    font-size: 14px;
+    color: #ffffff66;
+`;
+
+
