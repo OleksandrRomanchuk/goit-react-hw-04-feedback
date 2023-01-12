8f4e00440a38ffd79afdca5cab37d2141ea6eac7
Oleksandr Romanchuk <novator0511@gmail.com>
2023-01-12 16:00:40 +0200

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,16 +1,35 @@
 import PropTypes from 'prop-types';
+import {
+    FeedbackList,
+    FeedbackStatList,
+} from './Statistics.styled';
 
 export function Statistics({good, neutral, bad, total, positivePercentage}) {
     return <>
-                <ul>
-                    <li><p>Good:<span> {good}</span></p></li>
-                    <li><p>Neutral:<span> {neutral}</span></p></li>
-                    <li><p>Bad:<span> {bad}</span></p></li>
-                    </ul>
-                <ul>
+                <FeedbackList>
+                    <li>
+                        <p>
+                            Good:
+                            <span> {good}</span>
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            Neutral:
+                            <span> {neutral}</span>
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            Bad:
+                            <span> {bad}</span>
+                        </p>
+                    </li>
+                </FeedbackList>
+                <FeedbackStatList>
                     <li><p>Total:<span> {total}</span></p></li>
                     <li><p>Positiv feedback:<span> {positivePercentage}%</span></p></li>
-                </ul>
+                </FeedbackStatList>
             </>
 }
 
