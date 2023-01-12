e8d75a06f235c52a607cde83c346116b94d18dd8
Oleksandr Romanchuk <novator0511@gmail.com>
2023-01-12 16:00:46 +0200

    Update App.styled.jsx

--- src/components/App/App.styled.jsx
+++ src/components/App/App.styled.jsx
@@ -2,11 +2,18 @@ import styled from "styled-components";
 
 export const FeedbackApp = styled.div`
     display: grid;
+    grid-template-columns: repeat(1, minmax(320px, auto));
+    gap: 12px;
     justify-content: center;
-    align-items: flex-start;
+    align-items: center;
 
     width: 100vw;
     height: 100vh;
 
     text-align: center;
+    color: #ffffff;
+
+    @media(min-width: 650px) {
+    grid-template-columns: repeat(2, minmax(320px, auto));
+  }
 `;
