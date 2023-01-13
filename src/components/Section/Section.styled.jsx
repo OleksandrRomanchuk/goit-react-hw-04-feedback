568587409fb9cebda415cb3e4548547bb450c952
Oleksandr Romanchuk <novator0511@gmail.com>
2023-01-13 10:01:32 +0200

    Update Section.styled.jsx

--- src/components/Section/Section.styled.jsx
+++ src/components/Section/Section.styled.jsx
@@ -24,5 +24,5 @@ export const TitleSpan = styled.span`
     font-weight: 600;
     text-transform: uppercase;
     color: #ffffff;
-    text-shadow: 0 0 5px #FFF, 0 0 15px #FFF, 0 0 20px #da37fb, 0 0 30px #da37fb, 0 0 40px #da37fb, 0 0 55px #da37fb, 0 0 75px #da37fb, 2px 2px 2px rgba(206,89,55,0);
+    text-shadow: 0 0 5px #FFF, 0 0 15px #FFF, 0 0 20px #33f918, 0 0 30px #33f918, 0 0 40px #33f918, 0 0 55px #33f918, 0 0 75px #33f918, 2px 2px 2px rgba(206,89,55,0);
 `;
